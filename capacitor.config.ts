f6115dbe841307587203d9715316fa5d87a632fe
Luis A <luisrlvarezs@gmail.com>
2026-01-01 21:07:13 -0400

    pages-ios-v3

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,14 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'findsend',
-  webDir: 'www'
+  webDir: 'www',
+  plugins: {
+    StatusBar: {
+      backgroundColor: "#0000",
+      style: "DARK",
+      overlaysWebView: false
+    }
+  }
 };
 
 export default config;
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { StatusBar, Style } from '@capacitor/status-bar';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +8,14 @@ import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
   imports: [IonApp, IonRouterOutlet],
 })
 export class AppComponent {
-  constructor() {}
+
+  constructor() {
+    this.setStatusBar();
+  }
+
+  async setStatusBar() { 
+    await StatusBar.setBackgroundColor({ color: '#0000' }); 
+    await StatusBar.setStyle({ style: Style.Default }); 
+  }
+
 }
